fdcde4d53816f9e79af4b25dd94533c9562b2dc4
mmiche01 <mmiche01@students.uni-mainz.de>
2022-06-09 18:27:48 +0200

    Add temporary content for missing island quiz

---
+++ script_islands.js
@@ -0,0 +1,31 @@
+const selection = document.getElementById('section-selection');
+const container = document.getElementsByClassName('container')[0];
+const imageSection = document.createElement('section');
+const heading = document.createElement('h1');
+const islands = [
+	'Mallorca',
+	'Lesbos',
+	'Euboia',
+	'Corfu',
+	'Teneriffa',
+	'Fuerteventura',
+	'Iceland',
+	'Malta',
+];
+
+selection.classList.add('hidden');
+imageSection.classList.add('temp-img-display');
+heading.classList.add('temp-heading');
+heading.innerText = 'Stay tuned...';
+container.append(heading);
+container.append(imageSection);
+
+islands.forEach((item, index) => {
+	const imgContainer = document.createElement('div');
+	const svgElement = document.createElement('img');
+	imgContainer.classList.add('svg');
+	svgElement.id = `island-svg-${index + 1}`;
+	svgElement.src = `data/svg/islands/${item}.svg`;
+	imgContainer.append(svgElement);
+	imageSection.append(imgContainer);
+});
